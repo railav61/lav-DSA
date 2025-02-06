d80c3fa104ceea95189e43e34e41b17e02ccc877
Mritunjay Rai <146355352+railav61@users.noreply.github.com>
2025-02-06 19:44:24 +0530

    Time: 376 ms (20.38%), Space: 84.9 MB (67.71%) - LeetHub

---
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int nC2(int n) { return (n * (n - 1)) / 2; }
+
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> freq; // K->product...V->frequency of such pair
+        for (int i = 0; i < n - 1; ++i)
+            for (int j = i + 1; j < n; ++j)
+                freq[nums[i] * nums[j]]++;
+
+        int count = 0;
+        for (auto [product, frequency] : freq)
+            count += 8 * nC2(frequency);
+
+        return count;
+    }
+};
