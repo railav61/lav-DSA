adaf70a1a7bcce93288b8ab9b8a7ea21e992c2ce
Mritunjay Rai <146355352+railav61@users.noreply.github.com>
2025-08-18 10:41:45 +0530

    Time: 98 ms (12.55%), Space: 38.4 MB (8.59%) - LeetHub

---
+++ 0679-24-game/0679-24-game.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+vector<double> getValidOperations(double a, double b){
+    vector<double> validOperations;
+    validOperations.push_back(a+b);
+    validOperations.push_back(a*b);
+    validOperations.push_back(a-b);
+    validOperations.push_back(b-a);
+    if(abs(b)>FLT_EPSILON)
+    validOperations.push_back(a/b);
+    if(abs(a)>FLT_EPSILON)
+    validOperations.push_back(b/a);
+
+    return validOperations;
+}
+
+bool ispossible(vector<double> &arr){
+    int n = (int)arr.size();
+    if(n==1){
+        return abs(24.0 - arr[0]) <= FLT_EPSILON;
+    }
+
+    for(int fn = 0; fn < n; fn++){
+        for(int sn = 0;sn < n; sn++){
+            if(sn == fn){
+                continue;
+            }
+
+            double firstNumber = arr[fn];
+            double secondNumber = arr[sn];
+            vector<double> validOperations = getValidOperations(firstNumber,secondNumber);
+            vector<double> nextArr;
+            for(auto validOperation : validOperations){
+                nextArr.clear();
+                nextArr.push_back(validOperation);
+
+                for(int i =0; i< n;i++){
+                    if(i==fn || i==sn)
+                    continue;
+                    nextArr.push_back(arr[i]);
+                }
+
+                if(ispossible(nextArr))
+                return true;
+            }
+        }
+    }
+    return false;
+}
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> arr(cards.begin(),cards.end());
+        return ispossible(arr);
+    }
+};
